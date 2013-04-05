fac80de43842c2f525660710441660abd2b7282e
Jacky Chan <libing.chen@gmail.com>
2013-04-05 23:28:51 +0800

    weixin message

---
+++ robot-sdk/src/main/java/org/mvnsearch/wx/WeixinMessage.java
@@ -0,0 +1,156 @@
+package org.mvnsearch.wx;
+
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.input.SAXBuilder;
+
+import java.io.InputStream;
+import java.io.StringReader;
+
+/**
+ * weixin message
+ *
+ * @author linux_china
+ */
+public class WeixinMessage {
+    /**
+     * message id
+     */
+    private String id;
+    /**
+     * sender
+     */
+    private String sender;
+    /**
+     * receiver
+     */
+    private String receiver;
+    /**
+     * created time from 1970, unit is second
+     */
+    private Long createdTime;
+    /**
+     * type: text, image, location, link
+     */
+    private String type;
+    /**
+     * content, such as text, image url, link etc
+     */
+    private String content;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public Long getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Long createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    /**
+     * parse xml to get weixin message
+     *
+     * @param xmlText xml text
+     * @return weixin message
+     * @throws Exception exception
+     */
+    public static WeixinMessage parseXML(String xmlText) throws Exception {
+        SAXBuilder saxBuilder = new SAXBuilder();
+        Document document = saxBuilder.build(new StringReader(xmlText));
+        return constructMsg(document);
+    }
+
+    /**
+     * parse xml to get weixin message
+     *
+     * @param inputStream xml text
+     * @return weixin message
+     * @throws Exception exception
+     */
+    public static WeixinMessage parseXML(InputStream inputStream) throws Exception {
+        SAXBuilder saxBuilder = new SAXBuilder();
+        Document document = saxBuilder.build(inputStream);
+        return constructMsg(document);
+    }
+
+    /**
+     * construct weixin message from xml document
+     *
+     * @param document xml document
+     * @return weixin message
+     */
+    private static WeixinMessage constructMsg(Document document) {
+        Element rootElement = document.getRootElement();
+        WeixinMessage msg = new WeixinMessage();
+        msg.id = rootElement.getChildTextTrim("MsgId");
+        msg.receiver = rootElement.getChildTextTrim("ToUserName");
+        msg.sender = rootElement.getChildTextTrim("FromUserName");
+        msg.type = rootElement.getChildTextTrim("MsgType");
+        msg.content = rootElement.getChildTextTrim("Content");
+        String createTime = rootElement.getChildTextTrim("CreateTime");
+        if (createTime != null) {
+            msg.createdTime = Long.valueOf(createTime);
+        }
+        return msg;
+    }
+
+
+    /**
+     * get response xml
+     *
+     * @param type    type
+     * @param content content
+     * @return response xml
+     */
+    public String getResponseXml(String type, String content) {
+        String template = "<xml>\n" +
+                "    <ToUserName><![CDATA[%s]]></ToUserName>\n" +
+                "    <FromUserName><![CDATA[%s]]></FromUserName>\n" +
+                "    <CreateTime>%s</CreateTime>\n" +
+                "    <MsgType><![CDATA[%s]]></MsgType>\n" +
+                "    <Content><![CDATA[%s]]></Content>\n" +
+                "    <FuncFlag>0</FuncFlag>\n" +
+                "</xml>";
+        return String.format(template, sender, receiver, String.valueOf(System.currentTimeMillis() / 1000), type, content);
+    }
+
+}
