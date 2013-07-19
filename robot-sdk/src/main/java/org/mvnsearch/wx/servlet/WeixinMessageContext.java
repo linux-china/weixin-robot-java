9d81e6b0a1d4ccfe9116403150f0aadd19009c86
Jacky Chan <libing.chen@gmail.com>
2013-07-19 13:52:03 +0800

    weixin message context

--- robot-sdk/src/main/java/org/mvnsearch/wx/servlet/WeixinMessageContext.java
+++ robot-sdk/src/main/java/org/mvnsearch/wx/servlet/WeixinMessageContext.java
@@ -2,6 +2,8 @@ package org.mvnsearch.wx.servlet;
 
 import org.mvnsearch.wx.WeixinMessage;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -9,47 +11,71 @@ import java.util.Map;
  *
  * @author linux_china
  */
+@SuppressWarnings("unchecked")
 public class WeixinMessageContext {
     /**
-     *
+     * empty map
      */
-    private Map<String, String> query;
+    private static Map<String, String> emptyMap = Collections.emptyMap();
+    /**
+     * thread local variable
+     */
+    private final static ThreadLocal<Map<String, Object>> threadLocal = new ThreadLocal<Map<String, Object>>() {
+        protected Map<String, Object> initialValue() {
+            return new HashMap<String, Object>();
+        }
+    };
 
     /**
-     * weixin message
-     */
-    private WeixinMessage message;
-
-    /**
-     * construct method
-     */
-    public WeixinMessageContext() {
-
-    }
-
-    /**
-     * construct method
+     * set query string
      *
      * @param queryString query string
-     * @param message     message
      */
-    public WeixinMessageContext(String queryString, WeixinMessage message) {
-        this.message = message;
+    public static void setQueryString(String queryString) {
+        if (queryString != null && queryString.contains("=")) {
+            Map<String, String> query = new HashMap<String, String>();
+            for (String pair : queryString.split("&")) {
+                String[] parts = pair.split("=", 2);
+                if (parts.length == 2 && !parts[1].isEmpty()) {
+                    query.put(parts[0], parts[1]);
+                }
+            }
+            threadLocal.get().put("query", query);
+        }
     }
 
-    public Map<String, String> getQuery() {
-        return query;
+    /**
+     * get query
+     *
+     * @return query
+     */
+    public static Map<String, String> getQuery() {
+        Map<String, String> query = (Map<String, String>) threadLocal.get().get("query");
+        return query == null ? emptyMap : query;
     }
 
-    public void setQuery(Map<String, String> query) {
-        this.query = query;
+    /**
+     * set weixin message
+     *
+     * @param message message
+     */
+    public static void setWeixinMessage(WeixinMessage message) {
+        threadLocal.get().put("message", message);
     }
 
-    public WeixinMessage getMessage() {
-        return message;
+    /**
+     * get weixin message
+     *
+     * @return weixin message
+     */
+    public static WeixinMessage getWeixinMessage() {
+        return (WeixinMessage) threadLocal.get().get("message");
     }
 
-    public void setMessage(WeixinMessage message) {
-        this.message = message;
+    /**
+     * clear context
+     */
+    public static void clear() {
+        threadLocal.remove();
     }
 }
